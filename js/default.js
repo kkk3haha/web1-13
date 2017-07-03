716c70042a14f25d2bdcec45783681cef4bb2b8f
kkk3haha <lieutenantcommander.mystical@gmail.com>
2017-07-03 18:34:05 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -108,17 +108,17 @@ var clock=function(){
 }
 
 var update = function(){
-var now = new Date();
-var target = Date(2020,7,24,0,0,0,0);
-var diff = target.getTime() - now.getTime();
-var counter = separate_time(diff);
-document.getEleentById('countdown').textContent =
-  '東京オリンピックまであと　' +
-  counter[3] + '日' +
-  counter[2] + '時間' +
-  counter[1] + '分' +
-  counter[0] + '秒' ;
-refresh_up();
+  var now = new Date();
+  var target = new Date(2020,7,24,0,0,0,0);
+  var diff = target.getTime() - now.getTime();
+  var counter = separate_time(diff);
+  document.getEleentById('countdown').textContent =
+   '東京オリンピックまであと　' +
+    counter[3] + '日' +
+    counter[2] + '時間' +
+    counter[1] + '分' +
+    counter[0] + '秒' ;
+  refresh_up();
 }
 
 var refresh_up = function(){
